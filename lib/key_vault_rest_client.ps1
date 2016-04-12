4be7cf96d58d1483cc07af941a718a83077de827
Eric Nelson <e82.eric@gmail.com>
2016-04-12 00:00:26 -0700

    Added support for adal headless user credenital to key_vault_rest_client

--- lib/key_vault_rest_client.ps1
+++ lib/key_vault_rest_client.ps1
@@ -18,20 +18,19 @@ $ErrorActionPreference = "stop"
 . "$($restLibDir)\config.ps1"
 . "$($utilsDir)\announcer.ps1"
 
-function new_key_vault_rest_client {
-	param(
-		[ValidateNotNullOrEmpty()]$name=$(throw "name is mandatory"),
-		[ValidateNotNullOrEmpty()]$aadTenantId = $(throw "aadTenantId is mandatory"),
-		$loginHint,
-		$cacheIdentifier,
-		$defaultVersion = $(__.azure.rest.get_config "management_version"),
-		$defaultScheme = $(__.azure.rest.get_config "scheme"),
-		$defaultRetryCount = $(__.azure.rest.get_config "retry_count"),
-		$defaultContentType = $(__.azure.rest.get_config "management_content_type"),
-		$defaultTimeout = $(__.azure.rest.get_config "timeout"),
-		$fileTokenCachePath = "$env:userprofile\aad_tokens.dat",
-		$announcer = (new_announcer)
-	)
+function new_key_vault_rest_client { param(
+	[ValidateNotNullOrEmpty()]$name=$(throw "name is mandatory"),
+	[ValidateNotNullOrEmpty()]$aadTenantId = $(throw "aadTenantId is mandatory"),
+	$loginHint,
+	$cacheIdentifier,
+	$defaultVersion = $(__.azure.rest.get_config "management_version"),
+	$defaultScheme = $(__.azure.rest.get_config "scheme"),
+	$defaultRetryCount = $(__.azure.rest.get_config "retry_count"),
+	$defaultContentType = $(__.azure.rest.get_config "management_content_type"),
+	$defaultTimeout = $(__.azure.rest.get_config "timeout"),
+	$fileTokenCachePath = "$env:userprofile\aad_tokens.dat",
+	$announcer = (new_announcer)
+)
 
 	if($null -eq $cacheIdentifier) {
 		$cacheIdentifier = "$name`_key_vault"
@@ -43,6 +42,46 @@ function new_key_vault_rest_client {
 	} else {
 		$aadTokenProvider = new_aad_token_provider_with_login $aadResource $aadTenantId -LoginHint $loginHint
 	}
+	$authenticationPatcher = new_aad_file_cache_token_provider $cacheIdentifier $aadTenantId $aadResource $aadTokenProvider $fileTokenCachePath -Announcer $announcer
+	$requestHandler = new_request_handler (new_request_builder $announcer) (new_retry_handler $write_response $announcer) $announcer
+
+	$baseOptionsPatcher = new_simple_options_patcher `
+		$defaultRetryCount `
+		$defaultScheme `
+		$defaultContentType `
+		$defaultTimeout
+
+	$optionsPatcher = new_resource_manager_options_patcher `
+		$authenticationPatcher `
+		$baseOptionsPatcher `
+		"$($name).vault.azure.net"
+
+	$obj = new_rest_client $requestHandler $optionsPatcher $authenticationPatcher
+	$obj
+}
+
+function new_key_vault_rest_client_with_adal_user_credential { param(
+	[ValidateNotNullOrEmpty()]$name=$(throw "name is mandatory"),
+	[ValidateNotNullOrEmpty()]$aadTenantId = $(throw "aadTenantId is mandatory"),
+	[ValidateNotNullOrEmpty()][Security.SecureString]$loginHint = $(throw "loginHint is mandatory"),
+	[ValidateNotNullOrEmpty()][Security.SecureString]$password = $(throw "password is mandatory"),
+	$cacheIdentifier,
+	$defaultVersion = $(__.azure.rest.get_config "management_version"),
+	$defaultScheme = $(__.azure.rest.get_config "scheme"),
+	$defaultRetryCount = $(__.azure.rest.get_config "retry_count"),
+	$defaultContentType = $(__.azure.rest.get_config "management_content_type"),
+	$defaultTimeout = $(__.azure.rest.get_config "timeout"),
+	$fileTokenCachePath = "$env:userprofile\aad_tokens.dat",
+	$announcer = (new_announcer)
+)
+
+	if($null -eq $cacheIdentifier) {
+		$cacheIdentifier = "$name`_key_vault"
+	}
+
+	$aadResource = "https://vault.azure.net"
+	$aadTokenProvider = new_aad_token_provider_with_user_credential $aadResource $aadTenantId -LoginHint $loginHint -Password $password
+
 	$authenticationPatcher = new_aad_file_cache_token_provider $cacheIdentifier $aadTenantId $aadResource $aadTokenProvider $fileTokenCachePath -Announcer $announcer
 	$requestHandler = new_request_handler (new_request_builder $announcer) (new_retry_handler $write_response $announcer) $announcer
 
